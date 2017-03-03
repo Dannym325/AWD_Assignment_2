8ed3e727c284f6e0213dff648c0573555ffab6d1
u1477073 <u1477073@unimail.hud.ac.uk>
2017-03-03 13:28:09 +0000

    Added a log in view and changed to route to make it  go to the login page

---
+++ Assignment2/resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Log in</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+      <form name="loginFrm" action="" method="POST">
+        <p>Username:</p><input type="text" name="username">
+        <p>Password:</p><input type="password" name="password">
+        <input type="submit" name="loginSubmit" value="Log In">
+      </form>
+    </body>
+</html>
