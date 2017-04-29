c7a8ade5fea9843ab305edc07b519b8f4ca7f0fc
u1477073 <u1477073@unimail.hud.ac.uk>
2017-04-29 15:32:45 +0100

    Finished the bootstrap on the index page

--- Assignment2/resources/views/blogs/index.blade.php
+++ Assignment2/resources/views/blogs/index.blade.php
@@ -6,7 +6,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <title>Blogs</title>
 
         <!-- Fonts -->
@@ -14,12 +19,13 @@
 
     </head>
     <body>
+      <div class="container">
       <h1>Blogs</h1>
-      <button type="button"><a href="/create">New Blog!</a></button>
-
+      <button type="button"><a href="/create">New Blog</a></button>
+    </br></br>
       <p> Filter by category: </p>
     <form name="categoryFrm" action="/filter" method="POST">
-      <select name="filterSelect">
+      <select name="filterSelect" class="form-control">
         <option value="0">All</option>
         <option value="1">Cars</option>
         <option value="2">Day to day</option>
@@ -27,17 +33,15 @@
         <option value="4">Other</option>
       </select>
       <input type="hidden" name="_token" value="{{{ csrf_token() }}}" /> <!-- WHY? -->
+    </br>
       <input type="submit" name="filterSub" value="Search">
     </form>
   </br></br>
-
-      <table border="1">
+  <table class="table">
         <tr>
-          <th>ID </th> <!-- This can be deleted (or hidden )-->
           <th>Title</th>
           <th>Content</th>
           <th>User</th>
-          <th>CatID</th>
           <th>View</th>
           <th>Edit</th>
           <th>Delete</th>
@@ -45,11 +49,9 @@
       @foreach ($blogs as $blog)
         <div>
             <tr>
-              <td> {{ $blog->id }} </td>
               <td> {{ $blog->title }} </td>
               <td> {{ $blog->contents }} </td>
               <td> {{ $blog->username }} </td>
-              <td> {{ $blog->categoryID}} </td> <!-- Delete this -->
               <td> <a href="blogs/{{ $blog->id }}"> View </a> </td>
               <td> <a href="blogs/{{ $blog->id }}/edit"> Edit </a> </td>
               <td> <a href="/blogs/{{ $blog->id }}/delete"> Delete </a> </td>
@@ -58,6 +60,6 @@
         </div>
       @endforeach
       </table>
-
+    </div>
     </body>
 </html>
